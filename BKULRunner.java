7b454d8d5146b5f82c7ff7cfb76cb57397409cb4
githubcyclist <mouse869@gmail.com>
2017-02-26 09:25:17 -0500

    Update BKULRunner.java

--- BKULRunner.java
+++ BKULRunner.java
@@ -1,5 +1,5 @@
 public class BKULRunner {
-  public static void run(String server, ServerJoinCallback s) {
+  public static void run(String server, ServerJoinCallback s) throws Exception {
     s.joinServer(server);
   }
 }
